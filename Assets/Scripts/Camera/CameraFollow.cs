d04ba7517431fe43786eaa3c6a192c86aa540a78
Jeff Polasz <jeff_polasz@hotmail.com>
2018-04-03 22:56:55 -0400

    Added CameraFollow script and custom cursor

---
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+	public float followHeight = 8f;
+	public float followDistance = 6f;
+
+	private Transform player;
+
+	private float targetHeight;
+	private float currentHeight;
+	private float currentRotation;
+
+	void Awake () {
+		player = GameObject.FindGameObjectWithTag ("Player").transform;
+	}
+
+	void Update () {
+		targetHeight = player.position.y + followHeight;
+
+		currentRotation = transform.eulerAngles.y;
+		currentHeight = Mathf.Lerp (transform.position.y, targetHeight, 0.9f * Time.deltaTime);
+
+		Quaternion euler = Quaternion.Euler (0f, currentRotation, 0);
+		Vector3 targetPosition = player.position - (euler * Vector3.forward) * followDistance;
+
+		targetPosition.y = currentHeight;
+
+		transform.position = targetPosition;
+		transform.LookAt (player);
+	}
+
+} // CameraFollow
---
+++ Assets/Scripts/Camera/MouseScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseScript : MonoBehaviour {
+
+	public Texture2D cursorTexture;
+//	public GameObject mousePoint;
+
+	private CursorMode mode = CursorMode.ForceSoftware;
+	private Vector2 hotSpot = Vector2.zero;
+
+	void Start () {
+		
+	}
+
+	void Update () {
+		Cursor.SetCursor (cursorTexture, hotSpot, mode);
+	}
+
+} // MouseScript
