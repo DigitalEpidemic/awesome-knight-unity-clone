35f3f16dbd9f79f01a19a1578617fe0a4f8e6976
Jeffrey Polasz <jeff_polasz@hotmail.com>
2018-04-13 22:17:36 -0400

    Added enemy health and player skill damage

---
+++ Assets/Scripts/Enemy/EnemyHealth.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour {
+
+    public float health = 100f;
+    
+    public void TakeDamage (float amount) {
+        health -= amount;
+
+        print ("Enemy took damage! Health is " + health);
+
+        if (health <= 0) {
+
+        }
+    }
+
+} // EnemyHealth
---
+++ Assets/Scripts/FX/SkillDamage.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkillDamage : MonoBehaviour {
+
+    public LayerMask enemyLayer;
+    public float radius = 0.5f;
+    public float damageCount = 10f;
+
+    private EnemyHealth enemyHealth;
+    private bool collided;
+    
+	void Update () {
+        Collider[] hits = Physics.OverlapSphere (transform.position, radius, enemyLayer);
+
+        foreach (Collider c in hits) {
+            //if (c.isTrigger) {
+            //    continue;
+            //}
+            enemyHealth = c.gameObject.GetComponent<EnemyHealth> ();
+            collided = true;
+
+            if (collided) {
+                enemyHealth.TakeDamage (damageCount);
+                enabled = false;
+            }
+
+        }
+	}
+
+} // SkillDamage
