b7a6c876cb09ea6d75db082c7c3a86846cdee4e5
Jeffrey Polasz <jeff_polasz@hotmail.com>
2018-04-21 00:41:26 -0400

    Added BossFXDamage script

---
+++ Assets/Scripts/Enemy/BossFXDamage.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossFXDamage : MonoBehaviour {
+
+    public LayerMask playerLayer;
+    public float radius = 0.3f;
+    public float damageCount = 10f;
+
+    private PlayerHealth playerHealth;
+    private bool collided;
+    
+	void Update () {
+        Collider[] hits = Physics.OverlapSphere (transform.position, radius, playerLayer);
+
+        foreach (Collider c in hits) {
+            playerHealth = c.gameObject.GetComponent<PlayerHealth> ();
+            collided = true;
+
+            if (collided) {
+                playerHealth.TakeDamage (damageCount);
+                enabled = false;
+            }
+        }
+	}
+
+} // BossFXDamage
---
+++ Assets/Scripts/Enemy/BossSpecialAttacks.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossSpecialAttacks : MonoBehaviour {
+
+    public GameObject bossFire;
+    public GameObject bossMagic;
+
+    private Transform playerTarget;
+
+    void Awake () {
+        playerTarget = GameObject.FindGameObjectWithTag ("Player").transform;
+    }
+
+    void BossFireTornado () {
+        Instantiate (bossFire, playerTarget.position, Quaternion.Euler (0f, Random.Range (0f, 360f), 0f));
+    }
+
+    void BossSpell () {
+        Vector3 temp = playerTarget.position;
+        temp.y += 1.5f;
+        Instantiate (bossMagic, temp, Quaternion.identity);
+    }
+
+} // BossSpecialAttacks
